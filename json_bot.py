e531b02e1e240fffba70b7f67d7bee9ba6ccbea4
Valery Serykh <118994666+Valery-QA@users.noreply.github.com>
2022-12-11 17:53:27 +0200

    Add files via upload

---
+++ json_bot.py
@@ -0,0 +1,38 @@
+
+from telebot import TeleBot, types
+import json
+
+bot = TeleBot(token='А здесь положу свой токен', parse_mode='html')
+
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='Привет! Я умею проверять JSON и форматировать его в красивый текст\nВведи JSON в виде строки:',
+    )
+
+@bot.message_handler()
+def message_handler(message: types.Message):
+    try:
+        payload = json.loads(message.text)
+    except json.JSONDecodeError as ex:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f'При обработке произошла ошибка:\n<code>{str(ex)}</code>'
+        )
+        return
+    
+    text = json.dumps(payload, indent=2, sort_keys=True, ensure_ascii=False)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'JSON:\n<code>{text}</code>'
+    )
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
+
+#Бот-визитка Валерии Серых
